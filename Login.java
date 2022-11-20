806292b3f9b858be82d436a15ff50ba3eef8fb42
yuliantoprabowo <yuliantoprabowo23@gmail.com>
2022-11-20 14:11:36 +0700

    Initial commit

---
+++ Login.java
@@ -0,0 +1,26 @@
+package quizutamano1;
+
+import java.util.Scanner;
+
+public class Login {
+    private String ID="admin123";
+
+    public boolean doLogin(){
+        boolean isLogin=true;
+
+        String idInput="";
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Silahkan Masukan ID Anda");
+        System.out.println("=========================");
+        System.out.print("ID : ");
+        idInput = sc.nextLine();
+        if(!idInput.equalsIgnoreCase(ID)){
+            System.out.println("ID Anda Salah -> "+idInput);
+            isLogin=false;
+        }
+
+        return isLogin;
+    }
+
+}
